9a2a056ecef072fe84ea3a011e52085645376544
cjhp91 <cjhp91@gmail.com>
2017-02-06 23:15:23 -0800

    Latest

--- Assets/Scripts/Gameplay/Controllers/CarController.cs
+++ Assets/Scripts/Gameplay/Controllers/CarController.cs
@@ -9,19 +9,21 @@ namespace Castor
 	{
 		#region PUBLIC MEMBERS
 		public int CarID;
-		public float Acceleration = 2.0f;
-		public float MaxSpeed = 4.0f;
+		public float Acceleration = 7.0f;
+		public float MaxSpeed = 3.0f;
 		public float SpeedDecay = 0.1f;
+		public float Handling = 5.0f;
 
-		public bool Accelerating = false;
-		public bool Braking = false;
 		#endregion
 
 		#region PRIVATE MEMBERS
 		private float _speed;
 		private float _steering;
 		private float _throttle;
+		private float _brake;
 		private Vector3 _movementVector;
+
+		private const int _turnFactor = 30;
 		#endregion
 
 		#region PUBLIC METHODS
@@ -38,26 +40,31 @@ namespace Castor
 		#region UPDATE
 		private void Update()
 		{
-			this.transform.eulerAngles -= new Vector3(0,0,_steering);
+			if (_throttle > 0) {
+				if (_speed < MaxSpeed) {
+					_speed += Acceleration * Time.deltaTime * _throttle;
+				} else {
+					_speed = MaxSpeed;
+				}
+			} else {
+				if(_speed > 0)
+					_speed -= SpeedDecay * (1 + _brake);
+			}
+			if (_speed > 0) {
+				this.transform.eulerAngles -= new Vector3 (0, 0, _turnFactor * _steering * Handling * _speed * Time.deltaTime);
+				float theta = this.transform.eulerAngles.z * Mathf.Deg2Rad;
+				Vector3 dir = new Vector3 (-Mathf.Sin (theta), Mathf.Cos (theta), 0);
+				this.transform.position += dir * _speed * Time.deltaTime;
+			}
+
 		}
 		#endregion
 
 		#region INPUT
 		public void ReceiveDevice(InputDevice device)
 		{
-			if (device.RightTrigger.WasPressed) {
-			}
-			if (device.RightTrigger) {
-			}
-			if (device.RightTrigger.WasReleased) {
-			}
-
-			if (device.LeftTrigger.WasPressed) {
-			}
-			if (device.LeftTrigger) {
-			}
-			if (device.LeftTrigger.WasReleased) {
-			}
+			_throttle = device.Action1 ? 1 : 0;
+			_brake = device.Action2 ? 1 : 0;
 
 			if (device.Action1.WasPressed) {
 			}
@@ -72,6 +79,7 @@ namespace Castor
 			if (device.Action3.WasReleased) {
 			}
 			if (device.Action4.WasPressed) {
+				this.transform.position = Vector3.zero;
 			}
 			if (device.Action4.WasReleased) {
 			}
